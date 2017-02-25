0fb4a7490e6c08726865193c9708e9e9e393fa8f
Nelson Senna <onelsonsenna@gmail.com>
2017-02-25 00:35:19 -0300

    Create user feature
    This commit enables the creation of users in the system. It uses Rails'
    resources feature to add the needed routes (even if some of the
    generated routes are not proper handled).
    
    A normal user has the following attributes: name, email and password.
    The password is crypted using bcrypt by Rails to avoid storing plain
    text password.
    
    The new form uses Bootstrap framework because it's the most familiar
    one to me.
    
    Finally, in case of any error when creating a new user a flash message
    will be displayed showing the errors to the user and redirecting it to
    the form again.

--- presta_bridge/app/controllers/users_controller.rb
+++ presta_bridge/app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
   def new; end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+    else
+      flash[:error] = user.errors.full_messages
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+          .permit(
+            :name,
+            :email,
+            :password,
+            :password_confirmation
+          )
+  end
 end
--- presta_bridge/spec/controllers/users_controller_spec.rb
+++ presta_bridge/spec/controllers/users_controller_spec.rb
@@ -8,4 +8,44 @@ describe UsersController do
       expect(response).to render_template :new
     end
   end
+
+  describe 'POST #create' do
+    let(:valid_user_request) do
+      {
+        user: {
+          name: 'Foo',
+          email: 'foo@foo.com',
+          password: '12345',
+          password_confirmation: '12345'
+        }
+      }
+    end
+
+    let(:invalid_user_request) do
+      {
+        user: {
+          name: 'Foo',
+          email: 'foo@foo.com',
+          password: '',
+          password_confirmation: ''
+        }
+      }
+    end
+
+    context 'with valid params' do
+      it 'sets user id in session' do
+        expect { post :create, params: valid_user_request }.to change(User, :count)
+
+        expect(session[:user_id]).to_not be_nil
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'redirects to new' do
+        post :create, params: invalid_user_request
+
+        expect(response).to redirect_to new_user_url
+      end
+    end
+  end
 end
